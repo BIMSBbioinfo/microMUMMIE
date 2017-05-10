67681629eccb31ed4d6b99a75f957aaa43c06b89
dyusuf <dilmurat.yusuf@gmail.com>
2017-05-10 14:51:45 +0200

    new_run.sh - update

--- test_reorganization/output/new_run.sh
+++ test_reorganization/output/new_run.sh
@@ -1,4 +1,4 @@
-library='test_lib'
+library=$1
 
 preMicroMUMMIE.sh hsa.gff3 mature.fa AGO2_Z1.readcsv AGO2_Z1.groups.csv AGO2_Z1.distribution 100 1 $library
 microMUMMIE.pl ./$library.100.1/$library.100.miRNA.txt Genome.2bit AGO2_Z1.groups.csv AGO2_Z1.distribution AGO2_Z1.clusters.csv  $library $library.merged.gff  0 ../reference/orderedtc_scUTRs.txt ./
