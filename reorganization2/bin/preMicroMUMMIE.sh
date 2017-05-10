1a0309c7bed775d61dea83f0ae77186e421f0ed3
dyusuf <dilmurat.yusuf@gmail.com>
2017-05-10 12:50:34 +0200

    preMicroMUMMIE.sh - improvement

--- reorganization2/bin/preMicroMUMMIE.sh
+++ reorganization2/bin/preMicroMUMMIE.sh
@@ -5,50 +5,54 @@
 ##   MODIFY PATHS ACCORDINGLY    ##
 ###################################
 
-#Put matchMIRS.pl and filterGroupsforMUMMIE.pl in the scripts directory and indicate path below
-scripts=/data/ohler/Neel/bioTools/
-
 #Download miRNA gff from ftp://mirbase.org/pub/mirbase/CURRENT/genomes/hsa.gff3 and indicate path below
-mirnaGFF=/data/ohler/Neel/Genomes/human/hg19/annotation/v20/hsa.gff3
+mirnaGFF=$1
 
 #Download miRNA fasta from ftp://mirbase.org/pub/mirbase/CURRENT/mature.fa.gz and indicate path below
-mirnaFasta=/data/ohler/Neel/Genomes/human/hg19/annotation/v20/mature.fa
+mirnaFasta=$2
 
+readcsv=$3
 
+groups_csv=$4
+
+distribution=$5
+
+top=$6
+
+specif=$7
+
+library=$8
+
+output=$library.$top.$specif
 
 ###################################
 ## DO NOT MODIFY ANYTHING BELOW  ##
 ###################################
 
 cwd=$(pwd)
-gff=$(basename "$mirnaGFF");
-species=${gff%.*};
-grep -v  miRNA_primary_transcript $mirnaGFF > $cwd/$species.p.gff3;
-$cwd/$species.p.gff3;
-for file in $1
-do
-filename=$(basename "$file")
-library=${filename%.*}
-if [ ! -d "$cwd/$library.$2.$3" ]; then
-  mkdir "$cwd/$library.$2.$3";
+
+grep -v  miRNA_primary_transcript $mirnaGFF > $cwd/tmp.p.gff3;
+
+if [ ! -d "$cwd/$output" ]; then
+  mkdir "$cwd/$output";
 else
- rm $cwd/$library.$2.$3/*;
+ rm $cwd/$output/*;
 fi
 
 #calculate microRNA 5p/3p counts;
-grep miRNA "$cwd/$file" | awk 'BEGIN { FS = "," } ; { print $1"\t"$3-1"\t"$4"\t"$14"\t"$8"\t"$2 }' | intersectBed -s -wao -a stdin -b $cwd/$species.p.gff3 | sed 's/;/\t/g' | sed 's/Name=//g' | awk '$NF > 17'  | awk '{a[$17]+=$5}END{for (i in a) print i,a[i]}' | sort -rnk2 | head -$2 > $cwd/$library.$2.$3/$library.$2.count.txt
+grep miRNA "$cwd/$readcsv" | awk 'BEGIN { FS = "," } ; { print $1"\t"$3-1"\t"$4"\t"$14"\t"$8"\t"$2 }' | intersectBed -s -wao -a stdin -b $cwd/tmp.p.gff3 | sed 's/;/\t/g' | sed 's/Name=//g' | awk '$NF > 17'  | awk '{a[$17]+=$5}END{for (i in a) print i,a[i]}' | sort -rnk2 | head -$top > $cwd/$output/$library.$top.count.txt
 wait;
 
 #retrieve sequences in multiple formats for miRNAs found
-ruby -e 'first_line = true; while line = STDIN.gets; line.chomp!; if line =~ /^>/; puts unless first_line; print line[1..-1]; print " "; else; print line; end; first_line = false; end; puts' < $mirnaFasta  | awk 'BEGIN{FS=" "} {print $1 "\t" $6}' > $cwd/$library.$2.$3/tmp
+ruby -e 'first_line = true; while line = STDIN.gets; line.chomp!; if line =~ /^>/; puts unless first_line; print line[1..-1]; print " "; else; print line; end; first_line = false; end; puts' < $mirnaFasta  | awk 'BEGIN{FS=" "} {print $1 "\t" $6}' > $cwd/$output/tmp
 wait;
-perl $scripts/matchMIRS.pl $cwd/$library.$2.$3/tmp $cwd/$library.$2.$3/$library.$2.count.txt $cwd/$library.$2.$3/$library.$2;
+matchMIRS.pl $cwd/$output/tmp $cwd/$output/$library.$top.count.txt $cwd/$output/$library.$top;
 wait;
-rm $cwd/$library.$2.$3/tmp;
+rm $cwd/$output/tmp;
 
 #filter groups based on Conversion Specificity
-perl $scripts/filterGroupsforMUMMIE.pl $cwd/$library.groups.csv $3 | sed 's/Aligned to/Aligned_to/' > $cwd/$library.$2.$3/$library.groups.csv
-ln -s "$cwd/$library.distribution"  "$cwd/$library.$2.$3"
-rm $cwd/$species.p.gff3;
-done;
+filterGroupsforMUMMIE.pl $cwd/$groups_csv $specif | sed 's/Aligned to/Aligned_to/' > $cwd/$output/$library.groups.csv
+ln -s "$cwd/$distribution"  "$cwd/$output/$library.distribution"
+rm $cwd/tmp.p.gff3;
+
 exit 0;
